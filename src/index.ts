843c3757157f33c7ef9951edb57ff61a32cf7b3e
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-10-20 16:16:21 +0200

    Add Extractor class
    Code is now residing in an extractor class.
    With this class it is possible to search a document multiple times.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,68 @@
-import { Parser } from "commonmark";
+import { Parser, Node } from "commonmark";
 import * as fs from "fs";
 import * as path from "path";
 
-fs.readFile(path.join(".", "CHANGELOG.md"), "utf-8", (err, data) => {
-  if (err) throw err;
-  var reader = new Parser();
-  var parsed = reader.parse(data);
-  console.log(data);
-  var walker = parsed.walker();
-  var walterEvent;
-  while ((walterEvent = walker.next())) {
-    var node = walterEvent.node;
-    if (walterEvent.entering) {
-      //console.log(node);
-      if (node.type === "heading") {
-        if (node.firstChild) {
-          console.log(node.firstChild.literal);
+/**
+ * @class Class representing a parsed document for searching.
+ */
+class Extractor {
+  documentNode: Node;
+
+  /**
+   * Creates an instance of the Chapter extractor
+   * @param {string} document The document to be searched.
+   */
+  constructor(document: string) {
+    var reader = new Parser();
+    this.documentNode = reader.parse(document);
+  }
+
+  /**
+   * Searches a document for a heading that matches the headingText.
+   *
+   * @param {string} headingText Text to be matched in the heading.
+   * @param {number} headingLevel Level at which the heading resides in Markdown.
+   * @param {boolean} exact Should the text be matched exactly or search for contains.
+   */
+  public find(
+    headingText: string,
+    headingLevel: number,
+    exact: boolean = false
+  ): string {
+    var walker = this.documentNode.walker();
+    var walkerEvent;
+    while ((walkerEvent = walker.next())) {
+      var node = walkerEvent.node;
+      if (walkerEvent.entering) {
+        //console.log(node);
+        if (node.type === "heading" && node.level === headingLevel) {
+          if (node.firstChild) {
+            console.log(node.firstChild.literal);
+            if (exact) {
+              if (node.firstChild.literal === headingText) {
+                console.log("Found");
+              }
+            } else {
+              if (
+                node.firstChild.literal &&
+                node.firstChild.literal.includes(headingText)
+              ) {
+                console.log("Found");
+              }
+            }
+          }
+          console.log(node.level);
         }
-        console.log(node.level);
       }
     }
+    return "";
   }
+}
+
+module.exports = Extractor;
+
+fs.readFile(path.join(".", "CHANGELOG.md"), "utf-8", (err, data) => {
+  if (err) throw err;
+  var extractor = new Extractor(data);
+  extractor.find("v1.0.0", 2);
 });
