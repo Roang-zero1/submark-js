d14be70336e9a0be94c0d43274f9e8c7e9356923
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-11-16 01:06:20 +0100

    Add heading find option

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import markdown from "remark-parse";
 import stringify from "remark-stringify";
+import visit from "unist-util-visit";
+import { Node } from "unist";
+import builder from "unist-builder";
+
 import unified from "unified";
 
 /**
@@ -39,6 +43,48 @@ export class Extractor {
   ): string {
     var processor = unified()
       .use(markdown, this.parserSettings)
+      .use(() => {
+        return (tree) => {
+          var append = false;
+          var startNode: Node;
+          var startLevel = -1;
+          var nodes: Node[] = [];
+          visit(tree, (node) => {
+            console.log(node);
+            if (node.type === "heading") {
+              if (!startNode) {
+                if (
+                  typeof node.depth === "number" &&
+                  (headingLevel === 0 || node.depth === headingLevel)
+                ) {
+                  if (node.children instanceof Array) {
+                    if (node.children[0].value === headingText) {
+                      startNode = node;
+                      nodes.push(node);
+                      startLevel = node.depth;
+                      append = true;
+                    }
+                  }
+                }
+              } else {
+                if (
+                  typeof node.depth === "number" &&
+                  node.depth <= startLevel
+                ) {
+                  append = false;
+                }
+              }
+            } else {
+              if (append) {
+                nodes.push(node);
+              }
+            }
+          });
+          console.log(builder("root", nodes));
+          console.log(tree);
+          return builder("root", nodes);
+        };
+      })
       .use(stringify, this.stringifySettings);
 
     processor.process(this.document, (err, file) => {
